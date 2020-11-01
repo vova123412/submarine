9a756190c8bbbeae23c8365bd0795da60ae745a5
amirvatkin <varuss729@gmail.com>
2020-11-01 09:02:08 +0200

    refactor

--- client_sea_battle.py
+++ client_sea_battle.py
@@ -4,133 +4,93 @@ import time
 import threading
 
 # What is One_Player?
-class One_Player:
-    instance=None
+
     # this name is not informative
     # this is a GOD Class
     # you have to make some changes and divide your code!
-    class __Player:
-        def __init__(self):
-            self.value=""
-            self.flag=True# what is flag?
-            self.matrix= [0] * 100
-            self.threads=[]
-            self.P_List="";
-            HOST = '127.0.0.1' 
-            PORT = 65432
-            self.sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.sock.connect((HOST, PORT))
-            self.t = threading.Thread(target=self.recv,args=(self.sock,))
-            self.threads.append(t)
-            t.start()
-            time.Sleep(2000)
-
-        # What Data?
-        def Send_Data(self):
-            flag_s=True # what the meaning of flag_s?
-            while flag_s:
-                # x variable will die at the end of while loop
-                x=input("11-99  => 1,1 -9,9")
-                if(x>9 and x<100):
-                    flag_s=False
-                # Watch levels of abstractions
-                self.sock.send(str(x.encode('ascii')))
-            return x
-
-        def Init_Matrix(self):
-            for i in range(9):
-                self.matrix[i+1]=i+1
-            for i in range(9):
-                self.matrix[(i+1)*10]=i+1
-        # recv what?
-        def Recv(self):
-            flag=True
-            data=None
-            while flag:
-                data=self.sock.recv(1024)
-                if data:
-                    flag= False
-            return data
-
-        # the function called "Get..." but doesnt return anything
-        # the function is a Command not query
-        # the function is over 10 lines of code
-        def Get_Action(self,action):
-            location=0
-            while self.flag:
-                data=action
-                data=data.decode('ascii')
-                data=str(data)
-                if data:
-                    print("\n",data)
-                if data=="give me your ships":
-                    send_List(sock)
-                if data=="your turn":
-                    location=send_data(sock)
-                if data == "hit":
-                    matrix[location]=1
-                    p_matrix = [ matrix[i:i+10] for i in range(0,len(matrix),10) ]
-                    for i in p_matrix:
-                        print(i)
-                if data=="miss":
-                    matrix[location]=-1
-                    p_matrix = [ matrix[i:i+10] for i in range(0,len(matrix),10) ]
-                    for i in p_matrix:
-                        print(i)
-                if data=="win" or data=="lose":
-                    sock.close()
-                    self.flag=False
-               
 
 
-        # what list?
-        # what the purpose of value parameter?
-        def inList(self,List,value):
-            for i in List:
-                if i==value:
-                    return False
-            return True
-             
-        def init_List():
-            List=[]
-            length=0
-            while(length<2):
-                # initializing doesnt call of an input
-                x=input("place the ship ")
-                if(inList(List,x)):
-                    length=length+1
-                    List.append(x)
-                else:
-                    print("invalid number")
-            return List
-                
 
-        def Send_List(self,sock):
-            List=init_List()
-            P_List=pickle.dumps(List)
-            sock.send(P_List)
-            
-        def Print(self):
-            print(self.value)
-            
-        def Set_Value(self,value):
-            self.value=value
-            
+class Gmae:
+    def __init__(self,sock):
+        self.matrix= [0] * 100
+        self.sock=sock
+        
 
-    def __new__(cls):
-        if not One_Player.instance:
-            instance=One_Player.__Player
-        return instance
+
+    def Init_Matrix(self):
+        for i in range(9):
+            self.matrix[i+1]=i+1
+        for i in range(9):
+            self.matrix[(i+1)*10]=i+1
     
-    def __getattr__(self,name):
-        return (self.instance,name)
-    def __setattr__(self, name):
-        return setattr(self.instance,name)
+    def print_matrix(self):
+        Lmatrix =[ self.matrix[i:i+10] for i in range(0,len(self.matrix),10) ]
+        for i in Lmatrix:
+            print(i)
+
+    def Actions_loop(self):
+        while True:
+            action_strategy=pickle.loads(self.sock.recv(1024))     # recv what?   action
+            if action_strategy.do_action(self.sock)==0:
+                break
+        print("Game Over")
+
+
+
+        
+
+class ConnectionMannger:
+    def __init__(self):
+        self.value=""
+        self.flag=True# what is flag?
+        self.sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        mygame=Game(self.sock)
+        self.threads=[]
+        self.P_List=""
+        HOST = '127.0.0.1' 
+        PORT = 65432
+
+
+    # What Data?   -> in IAction
+
+
+    def Init_Connection(self):
+        
+        self.sock.connect((HOST, PORT))
+        self.mygame.Init_Matrix()
+        self.t = threading.Thread(target=self.mygame.Actions_loop,args=())
+        self.threads.append(t)
+        t.start()
+        time.Sleep(2000)
+
+
+
+
+    # the function called "Get..." but doesnt return anything
+    # the function is a Command not query
+    # the function is over 10 lines of code
+
+
+
+    # what list? -> moved to IAction
+    # what the purpose of value parameter?  moved to IAction: validation of ship_array
+            
+
+            # initializing doesnt call of an input   ->moved to IAction
+
+            
+
+
+
+
 
 
 def main():
-    player=One_Player()
-    player.recv(player)
+    player=ConnectionMannger()
+    player.Init_Connection()
+
+
     
 
 main()
--- server_sea_battle.py
+++ server_sea_battle.py
@@ -2,259 +2,263 @@ from random import randint
 import os
 import socket
 import json
-import threading
+from threading import Thread, Lock
 import pickle
 import time
-users=[]
-threads = []
-
-
+from Actions.IActions import *
 
 # what is this proxy for?
-# please revisit proxy pattern
+# please revisit proxy pattern 
 # maybe you could use a validator layer like decorator
-class Proxy:
 
-    def __init__(self,conn,player,index):
-        self.index=index
-        self.player=player
-        game=Game()
-        self.conn=conn
-        self.enemy_index
     
-    # this function is over 10 lines of code
-    def Validate_Board(self):
-        flag=True
-        while flag:
-            mesg="give me your ships"
-            self.conn.send(mesg.encode('ascii'));
-            try:         
-                ship_array =pickle.loads( self.conn.recv(1024))
-                flag = False
-                for i in ship_array:
-                    print(i)
-                    if i<=9 or i>100 or (i>9 and i%10==0):
-                        print("fail")
-                        flag=True
-            except:
-                mesg="error wrong type".encode('ascii')
-                flag=True
-                self.conn.send(mesg)
-        return ship_array
-
-    # this function is over 10 lines of code
-    def Validate_Coordinat(self,enemy_conn):
-        # watch levels of abstractions in this code
-        player_board=users[self.enemy_index]['List']
-        enemy_board= users[self.enemy_index]['List']
-        flag_s=True
-        coordinate
-        while flag_s:
-            # decoding and encoding are low level abstraction
-            coordinate=(int)(self.conn.recv(1024).decode('ascii'))
-            if(coordinate>9 and coordinate<100):
-                if(self.game.Attak(coordinate,enemy_board)):
-                   enemy_board.Remove(coordinate)
-                   if self.game.Win(enemy_board):
-                        mesg="win".encode("ascii")
-                        conn.send(mesg)
-                   else:
-                        mesg="hit".encode("ascii")
-                        conn.send(mesg)
-            coordinate=(int)(enemy_conn.recv(1024).decode('ascii'))
-            if(coordinate>9 and coordinate<100):
-                if(self.game.Attak(coordinate,player_board)):
-                   player_board.Remove(coordinate)
-                   if self.game.Win(player_board):
-                        mesg="win".encode("ascii")
-                        enemy_conn.send(mesg)
-                   else:
-                        mesg="hit".encode("ascii")
-                        enemy_conn.send(mesg)
-                    
-            
-        sock.send(str(x.encode('ascii')))
-        return x
-
-        print("validate coordinates")
-
-        
-    def Start():
-        self.game.Init_Game_Board(self.Validate_Board())
-        self.enemy_index=self.game.Wait(index,conn)
-        self.game.Status_In_Game(index,enemy_index)
-        self.game.in_game(users[index]['sock'],users[index]['List'],users[self.enemy_index]['sock'],users[self.enemy_index]['List'])
-        self.game.Status_Ready(index)
-        self.game.Status_Ready(enemy_index)
-
-    def Close_Game(self,index,index2):
-        users[index]['sock'].close()
-        users[index2]['sock'].close()
-
-
-    def recv_data(self,sock):
-        flag=True
-        while flag:
-            data=sock.recv(1024)
-            if data:
-                print(" \n the data is :",data)
-                return data
-                break
-
-        
-        
+    # this function is over 10 lines of code        
 
 # Game Facade, but what game are we playing?
-class Game:
-    global Users
-    def __init__():
-        self.string="asds"
+class FacadeSubmarinGame:
+    def __init__(player_one_sock,player_two_sock,player_one_idx,player_two_idx,prematch):
+        self.prematch=prematch
+        self.player_idx=player_idx
+        self.player2_idx=player2_idx
+        self.server=server
+        self.player_conn=player_conn
+        self.player_map=[]
+        self.player2_conn=player2_conn
+        self.player2_map=[]
+        self.action={
+        "Send_Ship":pickle.dumps(Send_Ship()),
+        "Turn":pickle.dumps(Turn()),
+        "Miss":Miss(),
+        "Hit":Hit(),
+        "Win":pickle.dumps(Win()),
+        "Lose":pickle.dumps(Lose()),
+        "Error":pickle.dumps(Error()),
+        }
+        self.player_map= self.init_game_board(self.player_conn)
+        self.player2_map=self.init_game_board(self.player2_conn)
+        self.in_game(self.player_conn,self.player_map,self.player2_conn,self.player2_map) 
 
-    def Init_Game_Board(self,player_board,user):
-        print("init boad")
-        users[user['index']]['List']=player_board
 
-    
-    # function name should contain a verb
-    def Status_Ready(self,user):
-        for i in users[user['index']]['List']:
-            print(i)
-        users[user['index']]['status']=1;
-        print("the status is" ,users[user['index']]['status'])
-        print("the index is" ,users[user['index']]['index'])
-        print("nice job")
-    
 
-    # using connection objects is not hiding 3d-party code like sockets
-    def play(self,conn,user):
-        global users
-        self.wait(user['index'],conn)
-        conn.close()
 
-    # wait for what?
-    def Wait(self,index,sock):
-        global users
-        index2=0
-        flag=True # what is flag?
-        while flag:
-            for i in users:
-                if i['status']==1 and i['index']!=index:
-                    flag=False
-                    index2=i['index']
-        print("connected two players")
-        self.Status_In_Game(index,index2)
-        return index2
+    def init_game_board(self,conn):
+        conn.send(self.action["Send_Ship"])
+        ship_array=pickle.loads(conn.recv(1024))
+        while validate_game_board(ship_array):
+            conn.send(self.action["Send_Ship"])
+            ship_array=pickle.loads(conn.recv(1024))
+
+
+    def validate_game_board(self,ship_array):
+        try:         
+            for i in ship_array:
+                if i<=9 or i>100 or (i>9 and i%10==0):
+                    print("fail")
+                    return False
+            return True
+        except:
+                conn.send(self.action["Error"])
+        return False
+
+
+
+    # using connection objects is not hiding 3d-party code like sockets -> moved to IAction
+
+
+   
+       
+  
         
        
     # the function doesnt contain a verb
-    def Status_In_Game(self,index,index2):
-        global users
-        users[index]['status']=2
-        users[index2]['status']=2
+    def endinggamestatus(self,index,index2):
+        self.prematch.changestatusendgame(index,index2)
 
         
-    def Player_One_Game(self,coordinate,board):
-        pass
 
-    def Win(self,board):
-        if len(board)==0:
-            return True
-        return False
-    
-    # command query seperation is violated!
-    # this function is not making any changes
-    def Attak(sekf,coordinate,board):
-        for pos in board:
-            if pos==coordinate:
-                return True
-        return False
-    
+
+    # this function is over 10 lines of code
     def in_game(self,sock,List,sock2,List2):
         print("\n \n",List,List2)
         while len(List)!=0 and len(List2)!=0:
             flag=False
-            sock.send("your turn")
+            sock.send(self.action["Turn"])
             data=int(self.recv_data(sock))
             for i in List2:
                 if data==i:
                     flag=True
-                    sock.send("hit")
+                    self.action["Hit"].set_coordinate(data)
+                    sock.send(pickle.dumps(self.action["Hit"]))
                     break
             if flag:
                 print("\n list 2 is :",List2)
                 List2.remove(data)
                 if(len(List2)==0):
-                    time.sleep(4)
-                    sock.send("win")
-                    sock2.send("lose")
-                    time.sleep(4)
+                    sock.send(self.action["Win"])
+                    sock2.send(self.action["Lose"])
                     break
             else:
-                sock.send("miss")
+                self.action["Miss"].set_coordinate(data)
+                sock.send(pickle.dumps(self.action["Miss"]))
             flag=False
             
 
-            
-            sock2.send("your turn")
+                     
+            sock2.send(self.action["Turn"])
             
             data=int(self.recv_data(sock2))
             for i in List:
                 if data==i:
                     flag=True
-                    sock2.send("hit")
+                    self.action["Hit"].set_coordinate(data)
+                    sock2.send(pickle.dumps(self.action["Hit"]))
                     break
             if flag:
                 print(len(List))
                 List.remove(data)
                 if len(List)==0:
-                    time.sleep(4)
-                    sock2.send("win")
-                    sock.send("lose")
-                    time.sleep(4)
+                    sock2.send(self.action["Win"])
+                    sock.send(self.action["Lose"])
                     break
                 
             else:
-                sock2.send("miss")
+                self.action["Miss"].set_coordinate(data)
+                sock2.send(pickle.dumps(self.action["Miss"]))
+        self.endinggamestatus()
 
+    def recv_data(self,sock):
+        while True:
+            data=sock.recv(1024).decode("ascii")
+            if data:
+                print(" \n the data is :",data)
+                return data
+                break  
+
+
+
+
+
+class PreMatch:
+    def __init__(self,users,threads):
+        self.users=users
+        self.threads=threads
+        self.semaphore =True
+
+    def threadfindmatch(self):
+        matches= Thread(target=self.findmatch)
+        self.threads.append(matches)
+        matches.start()
+
+    def findmatch(self):
+        while self.semaphore :
+            players_idx=self.searchforgames()
+            if players_idx !=None:
+                print(players_idx)
+                self.changestatusingame(players_idx[0],players_idx[1])
+                match=Thread(target=self.create_new_game,args=(players_idx[0],players_idx[1]))
+                self.threads.append(match)
+                match.start()
+
+
+    # function name should contain a verb
+    def changestatusingame(self,player_one_idx,player_two_idx):
+        self.users[player_one_idx]['status']=2
+        self.users[player_two_idx]['status']=2
+
+     # function name should contain a verb
+    def changestatusendgame(self,player_one_idx,player_two_idx):
+        self.users[player_one_idx]['status']=0
+        self.users[player_two_idx]['status']=0
+
+    def create_new_game(self,player_one_idx,player_two_idx):
+        one_vs_one=FacadeSubmarinGame(self.users[player_one_idx]['sock'],self.users[player_two_idx]['sock'],player_one_idx,player_two_idx,self)
         
 
+    def searchforgames(self):
+        while self.semaphore :
+            players_idx=[]
+            for i in self.users:
+                if i['status']==1:
+                    players_idx.append(i['index'])
+                if len(players_idx)==2:
+                    print("match found")
+                    return players_idx
 
-        
+
+
+class PlayerMannger:
+    def __init__(self,users):
+        self.users=users
+
+    def play(self,conn,user):
+        while self.semaphore :
+            if self.users[user['index']]['status']!=2 and self.users[user['index']]['status']!=1:
+                message=pickle.dumps(Options())
+                conn.send(message)
+                data= self.recv_data(conn)
+                print(data)
+                if data=="search":
+                    conn.send(pickle.dumps(WaitForOpponent()))
+                    self.searchgamestatus(user)
+                    print(self.users[user['index']]['status']) 
+                    time.sleep(3)
+
+    # watch levels of abstractions in this code
+    # decoding and encoding are low level abstraction
+                    
+    def recv_data(self,sock):
+        while True:
+            data=sock.recv(1024).decode("ascii")
+            if data:
+                print(" \n the data is :",data)
+                return data
+                break  
+
+
+    def searchgamestatus(self,user):
+        self.users[user['index']]['status']=1
 
 
 # what server?
-class Server:
+class SocketMannager:
     # the constructor is over 10 lines of code
     def __init__(self):
-        global users
-        global threads
-     
+        self.users=[]
+        self.threads=[]
+        HOST = ''  
+        PORT = 65432      
+        self.sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((HOST, PORT))
+        self.sock.listen(5)
 
-        HOST = '127.0.0.1' 
-        PORT = 65432        
-        # the names are not good you didn't think about them at all
-        sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind((HOST, PORT))
-        sock.listen(5)
-        index=0
-        flag=True
-        
-        while index<2: 
-            conn, addr = sock.accept()
-            print('Connected by', addr)
+
+        self.playermannger=PlayerMannger(self.users)
+        self.prematch=PreMatch(self.users,self.threads)
+    
+
+    def acceptplayers(self):
+        player_idx=0
+        while player_idx<5: 
+            conn, addr = self.sock.accept()
             user={
                 'status':0,
-                'index':index,
+                'index':player_idx,
                 'addr':addr,
                 'sock':conn,
-                'List':[]
                 }
-            index=index+1
-            users.append(user)
-            proxy=Proxy(conn,user)
-            t = threading.Thread(target=proxy.Start,args=())
-            threads.append(t)
-            t.start()
+            player_idx=player_idx+1
+            self.users.append(user)
+            self.threadplayermannger(conn)
+
+        self.semaphore =False
+        sys.exit() 
+    
+    def threadplayermannger(self,conn):
+        t = Thread(target=self.playermannger.play,args=(conn,user))
+        self.threads.append(t)
+        t.start()
+
 
 
 
@@ -263,4 +267,18 @@ class Server:
 
         
 
-a=Server()
+socketmannager=SocketMannager()
+socketmannager.prematch.threadfindmatch()
+socketmannager.acceptplayers()
+
+
+
+
+
+
+
+
+
+
+
+
